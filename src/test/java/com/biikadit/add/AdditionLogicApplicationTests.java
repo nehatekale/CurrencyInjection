0368c8878c19962811d1e345ea5a7ce8fff299e3
nehatekale <nehatekalepatil@gmail.com>
2023-04-18 20:33:15 +0530

    this is done by neha

---
+++ src/test/java/com/biikadit/add/AdditionLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biikadit.add;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdditionLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
